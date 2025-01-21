1b0818bf5f3853cc71c915850e53a08eaea1f046
Skynet Technologies <developer3@skynettechnologies.com>
2025-01-21 15:59:37 +0530

    read me updates

--- obj/Debug/net8.0/AllinOneAccessibility.AssemblyInfo.cs
+++ obj/Debug/net8.0/AllinOneAccessibility.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute(@"All in One Accessibility widget improves website ADA compliance and browser experience for ADA, WCAG 2.1 & 2.2, Section 508, Australian DDA, European EAA EN 301 549, UK Equality Act (EA), Israeli Standard 5568, California Unruh, Ontario AODA, Canada ACA, German BITV, France RGAA, Brazilian Inclusion Law (LBI 13.146/2015), Spain UNE 139803:2012, JIS X 8341 (Japan), Italian Stanca Act and Switzerland DDA Standards. All in One Accessibility Widget complies with GDPR, COPPA regulations.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb242038c9c3f92405895447d06914b96fbfa4fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("All in One Accessibility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllinOneAccessibility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
